f71a7f5bec1875afa42670ba32f6448dded86bb2
Sofia Tkachenko <duck.shelter@gmail.com>
2020-03-30 11:36:58 +0300

    Удалила лишние this
    Но сеттеры и геттеры - тихий ужас

--- app/src/main/java/com/example/innoapp/fragments/Event.java
+++ app/src/main/java/com/example/innoapp/fragments/Event.java
@@ -1,15 +1,14 @@
 package com.example.innoapp.fragments;
-//author Makar Shevchenko
+// author Makar Shevchenko
 import java.security.acl.Group;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 /*
-Class of events which contains information to generate event-cards
+ *  Класс событий для генерации карточек событий
  */
 public class Event {
-    //parameters
     private String name;
     private Date date;
     private HashMap<String, String> data;
@@ -17,50 +16,48 @@ public class Event {
     private boolean is_optional;
     private String place;
 
-    //clear constructor
     Event()
     {
-        this.name = "Event";
-        this.date = new Date();
-        this.place = "Innopolis";
-        this.is_optional = true;
+        name = "Event";
+        date = new Date();
+        place = "Innopolis";
+        is_optional = true;
     }
 
-    //constructor with inputs
     Event(String newName, Date newDate, HashMap<String, String> newData, LinkedList<Group> newGroups, boolean newIs_optional) {
-        this.setName(newName);
-        this.setDate(newDate);
-        this.setData(newData);
-        this.setGroups(newGroups);
-        this.setIs_optional(newIs_optional);
+        setName(newName);
+        setDate(newDate);
+        setData(newData);
+        setGroups(newGroups);
+        setIs_optional(newIs_optional);
     }
 
-    //setters
+    // setters
     public void setName(String newName) {
-        this.name = newName;
+        name = newName;
     }
 
     public void setDate(Date newDate) {
-        this.date = newDate;
+        date = newDate;
     }
 
     public void setData(HashMap<String,String> newData) {
-        this.data = newData;
+        data = newData;
     }
 
     public void setGroups(LinkedList<Group> newGroups) {
-        this.groups = newGroups;
+        groups = newGroups;
     }
 
     public void setIs_optional(boolean newIs_optional) {
-        this.is_optional = newIs_optional;
+        is_optional = newIs_optional;
     }
 
     public void setPlace(String newPlace) {
-        this.place = newPlace;
+        place = newPlace;
     }
 
-    //getters
+    // getters
     public String getName() {
         return name;
     }
