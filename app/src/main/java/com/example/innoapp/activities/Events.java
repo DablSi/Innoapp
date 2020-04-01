2a0b78bb357be5853420fd9553a594dd7363389b
Sofia Tkachenko <duck.shelter@gmail.com>
2020-04-01 13:19:58 +0300

    Translated comments and removed unnecessary ones

--- app/src/main/java/com/example/innoapp/activities/Events.java
+++ app/src/main/java/com/example/innoapp/activities/Events.java
@@ -16,9 +16,7 @@ import java.util.List;
 
 public class Events extends AppCompatActivity {
 
-    // Список карточек
     private List cards;
-    // Ссылка на RecyclerView в activity_events.xml
     private RecyclerView rv;
 
     @Override
@@ -26,22 +24,21 @@ public class Events extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
-        // взятие ссылки на RecyclerView в activity_events.xml
-        rv=(RecyclerView)findViewById(R.id.rv);
+        rv = (RecyclerView)findViewById(R.id.rv);
 
-        // Установка в RecyclerViev LayoutManager (список)
+        // set LayoutManager in RecycleView (list)
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
 
-        // Загрузка 10 карточек
+        // load 10 cards
         initializeData();
-        // Отображение крточек
+        // show cards
         initializeAdapter();
     }
 
     private void initializeData(){
         cards = new ArrayList<>();
-        //Название, дата, длительность, группы, кастомное, место, описание, количество посетителей
+        // Name, date, duration, groups, optionality, place, description, number of visitors
         Date d = new Date();
         cards.add(new Event("Card 1", "Content 1"));
         cards.add(new Event("Card 2", "Content 2"));
