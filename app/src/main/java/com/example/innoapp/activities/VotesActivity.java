85590c381748713ab522eed9016f371f6b27497e
cene655 <54323195+cene655@users.noreply.github.com>
2020-04-04 18:58:59 +0500

    VatesFull

--- app/src/main/java/com/example/innoapp/activities/VotesActivity.java
+++ app/src/main/java/com/example/innoapp/activities/VotesActivity.java
@@ -3,39 +3,89 @@ package com.example.innoapp.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.innoapp.R;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class VotesActivity extends Activity {
     public static LinkedList<Vote> vоtes;
     private LinearLayout voteLinearLayout;
     private int countID = 0;
     private boolean b1 = false;
-
+    String[] groupsS = {"Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_votes);
-        voteLinearLayout = findViewById(R.id.voteLinearLayout);
+        voteLinearLayout = (LinearLayout) findViewById(R.id.voteLinearLayout);
         vоtes = new LinkedList<Vote>();
-        vоtes.add(new Vote("Favourite season?", new String[]{"Winter", "Spring", "Summer", "Autumn"}, false));
-        CreateVotes(vоtes);
+        CreateSpinner(groupsS);
+
+    }
+    private void CreateSpinner(String[] sGroups)
+    {
+        TextView textViewSpinner = (TextView) findViewById(R.id.textViewSpinner) ;
+        Spinner groups = (Spinner) findViewById(R.id.groups);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, groupsS);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        groups.setAdapter(adapter);
+        textViewSpinner.setText(sGroups[0]);
+        //
+
+        //
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                voteLinearLayout.removeAllViews();
+                // Получаем выбранный объект
+                vоtes.removeAll(vоtes);
+                String item = (String)parent.getItemAtPosition(position);
+                textViewSpinner.setText(item);
+                switch (item){
+                    case("Group 1"):
+                        vоtes.add(new Vote("Favourite season?", new String[]{"Winter", "Spring", "Summer", "Autumn"}, false));
+                        vоtes.add(new Vote("Favourite season?", new String[]{"Winter", "Spring", "Summer", "Autumn"}, true));
+                        CreateVotes(vоtes);
+                        break;
+                    case("Group 2"):
+                        vоtes.add(new Vote("Favourite season124?", new String[]{"Win314ter", "Spr134ing", "Sum4mer", "Autumn"}, false));
+                        vоtes.add(new Vote("Favourite season1241?", new String[]{"Win134ter", "Spri134ng", "Sum134mer", "Aut4umn"}, true));
+                        CreateVotes(vоtes);
+                        break;
+                    case("Group 3"):
+                        vоtes.add(new Vote("favourite season?125125126236", new String[]{"Winter", "Spring", "Summer", "Autumn"}, false));
+                        vоtes.add(new Vote("Favourite season?", new String[]{"Winter", "Spring", "Summer", "Autumn"}, true));
+                        CreateVotes(vоtes);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        groups.setOnItemSelectedListener(itemSelectedListener);
     }
 
     private void CreateVotes(LinkedList<Vote> list) {
         for (Vote i : list) {
             ArrayList<RadioButton> radioButtonsArrayList = new ArrayList<RadioButton>(); // array for RadioButtons
+            ArrayList<CheckBox> checkBoxArrayList = new ArrayList<CheckBox>();
             int[] voted = new int[i.variants.length];
-
             RelativeLayout relativeLayout1 = new RelativeLayout(this);
             RelativeLayout.LayoutParams relativeLayout1Params = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             relativeLayout1Params.setMargins(0, 50, 0, 0);
@@ -60,7 +110,7 @@ public class VotesActivity extends Activity {
             relativeLayout2.addView(voteTextView);
             voteTextView.setTextColor(getResources().getColor(R.color.white));
             voteTextView.setId(TextView.generateViewId());
-
+            final int[] lastP = {-1};
             if (!i.multiple) {
                 RadioGroup voteRadioGroup = new RadioGroup(this);
                 RelativeLayout.LayoutParams voteRadioGroupParams = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
@@ -68,7 +118,7 @@ public class VotesActivity extends Activity {
                 voteRadioGroup.setLayoutParams(voteRadioGroupParams);
                 voteRadioGroup.setPadding(0, 10, 0, 25);
                 relativeLayout2.addView(voteRadioGroup);
-                final int[] lastP = {-1};
+
 
                 for (int j = 0; j < i.variants.length; j++) {
                     RadioButton voteRadioButton = new RadioButton(this);
@@ -79,8 +129,6 @@ public class VotesActivity extends Activity {
                     voteRadioButton.setTextSize(17);
                     voteRadioGroup.addView(voteRadioButton);
                     radioButtonsArrayList.add(voteRadioButton);
-                    voteRadioButton.setId(countID);
-                    countID++;
                     int finalI2 = list.indexOf(i);
                     int finalI = j;
                     voteRadioButton.setOnClickListener(new View.OnClickListener() {
@@ -103,6 +151,45 @@ public class VotesActivity extends Activity {
                 }
             } else {
                 // здесь ты делаешь CheckBox :^)
+                for (int j = 0; j < i.variants.length; j++) {
+                    CheckBox voteCheckBox = new CheckBox(this);
+                    RelativeLayout.LayoutParams voteCheckBoxParams = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    if (j > 0)
+                    {
+                        voteCheckBoxParams.addRule(RelativeLayout.BELOW, checkBoxArrayList.get(j - 1).getId());
+                    }
+                    else {
+                        voteCheckBoxParams.addRule(RelativeLayout.BELOW, voteTextView.getId());
+                    }
+                    voteCheckBox.setLayoutParams(voteCheckBoxParams);
+                    voteCheckBox.setText(i.variants[j]);
+                    voteCheckBox.setTextColor(getResources().getColor(R.color.white));
+                    voteCheckBox.setTextSize(17);
+                    relativeLayout2.addView(voteCheckBox);
+                    voteCheckBox.setId(CheckBox.generateViewId());
+                    checkBoxArrayList.add(voteCheckBox);
+                    int finalI2 = list.indexOf(i);
+                    int finalI = j;
+                    final boolean[] ch = {false};
+                    voteCheckBox.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if(ch[0]) {
+                                voted[finalI] = voted[finalI] - 1;
+                                ch[0] = false;
+                            }
+                            else{
+                                voted[finalI] = voted[finalI] + 1;
+                                ch[0] = true;
+                            }
+                            lastP[0] = finalI;
+                            for (int k = 0; k < list.get(finalI2).variants.length; k++) {
+                                checkBoxArrayList.get(k).setText(list.get(finalI2).variants[k] + "           " + voted[k]);
+                            }
+                        }
+                    });
+                }
+
             }
 
         }
