83a57b216a7bdbc26719b4b961b57cb26f1f8bdd
doldrums <doldrums.317@gmail.com>
2020-04-10 14:14:09 +0200

    1. added string res 2. minor insignificant amendments in xml

--- app/src/main/java/com/example/innoapp/activities/ProfileActivity.java
+++ app/src/main/java/com/example/innoapp/activities/ProfileActivity.java
@@ -5,24 +5,16 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.innoapp.R;
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class ProfileActivity extends AppCompatActivity {
     private String eMail;
-    private TextView mailTextView;
-    private TextView avaTextView;
-    private RelativeLayout languageLayout;
-    private RelativeLayout darkThemeLayout;
-    private RelativeLayout notificationsLayout;
-    private RelativeLayout logOutLayout;
+    //private TextView txt_language;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +24,13 @@ public class ProfileActivity extends AppCompatActivity {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         eMail = sp.getString(LoginActivity.EMAIL, "Cene655@mail.ru");
         SetMail();
+       // txt_language = findViewById(R.id.txt_lang);
+
     }
 
     private void SetMail() {
-        mailTextView = (TextView) findViewById(R.id.mailTextView);
-        avaTextView = (TextView) findViewById(R.id.avaTextView);
+        TextView mailTextView = findViewById(R.id.mailTextView);
+        TextView avaTextView = findViewById(R.id.avaTextView);
         mailTextView.setText(eMail);
         avaTextView.setText(Character.toString(eMail.toCharArray()[0]));
     }
