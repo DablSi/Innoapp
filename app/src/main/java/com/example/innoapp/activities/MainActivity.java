218fd0a4e096bec6ff170fea1d293f5237a6166e
Sofia Tkachenko <duck.shelter@gmail.com>
2020-04-11 14:59:56 +0300

    For the merge

--- app/src/main/java/com/example/innoapp/activities/MainActivity.java
+++ app/src/main/java/com/example/innoapp/activities/MainActivity.java
@@ -12,133 +12,100 @@ import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.viewpager.widget.ViewPager;
+import androidx.cardview.widget.CardView;
 
 import com.example.innoapp.R;
-import com.example.innoapp.adapter.TabsPagerFragmentAdapters;
-import com.example.innoapp.utils.EAN13CodeBuilder13;
-import com.github.chrisbanes.photoview.PhotoView;
-import com.google.android.material.tabs.TabLayout;
+import com.example.innoapp.utils.EAN13CodeBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
-
 
 import static com.example.innoapp.activities.LoginActivity.CODE;
 import static com.example.innoapp.activities.LoginActivity.LOGIN;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvBarcode;
+    int id = 1;
+
+    private TextView tvBarcode, txtDescriptionBarcode;
+    public static String code = "124958761310";
+    HashSet<String> groups;
+    private boolean barcodeScale = false;
     DatabaseReference mDatabase;
     private NotificationManager mNotificationManager;
 
-    public static String code;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         if (sp.getString(LOGIN, "").equals(""))
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        code = sp.getString(CODE, "");
+        code = sp.getString(CODE, "124958761310");
+        groups = (HashSet<String>) sp.getStringSet("GROUPS",new HashSet<String>());
         // barcode
         tvBarcode = findViewById(R.id.tvBarcode);
+        txtDescriptionBarcode = findViewById(R.id.txt_description_barcode);
+        FloatingActionButton fabSettings = findViewById(R.id.fab_setting);
+        CardView btnMaps = findViewById(R.id.btn_maps);
+        CardView btnFAQ = findViewById(R.id.btn_faq);
+        CardView btnSchedule = findViewById(R.id.btn_schedule);
+        CardView btnVoting = findViewById(R.id.btn_voting);
+        // set barcode's font
+        Typeface font = Typeface.createFromAsset(this.getAssets(), "fonts/EanP72TtNormal.ttf");
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        // tabs initialisation
-        initTabs();
-        // barcode font
-        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/EanP72TtNormal.ttf");
-        // sets the barcode
         tvBarcode.setTypeface(font);
         // sets barcode's value
-        EAN13CodeBuilder13 bb = new EAN13CodeBuilder13(code);
+        EAN13CodeBuilder bb = new EAN13CodeBuilder(code);
         tvBarcode.setText(bb.getCode());
         // barcode settings
-        RelativeLayout.LayoutParams layoutParamsBarcode = new RelativeLayout.LayoutParams(500, 350);
-        layoutParamsBarcode.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        layoutParamsBarcode.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        tvBarcode.setLayoutParams(layoutParamsBarcode);
-        tvBarcode.setPadding(0, 25, 0, 0);
+        tvBarcode.setPadding(0, 20, 20, 20);
+        fabSettings.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ProfileActivity.class)));
+        btnSchedule.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Events.class)));
+        btnVoting.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, VotesActivity.class)));
+        btnFAQ.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, FAQActivity.class)));
+        btnMaps.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, MapActivity.class)));
         planningPush();
     }
-
-    private void initTabs() {
-        TabLayout mapsTabLayout = findViewById(R.id.mapsTabLayout);
-        ViewPager mapsViewPager = findViewById(R.id.mapsViewPager);
-        TabsPagerFragmentAdapters adapters = new TabsPagerFragmentAdapters(getSupportFragmentManager());
-        mapsViewPager.setAdapter(adapters);
-        mapsTabLayout.setupWithViewPager(mapsViewPager);
-    }
-
-    boolean barcodeScale = false;
-
     // zooms barcode
     public void onButtonClickBarcode(View v) {
         if (barcodeScale) {
-            tvBarcode.setTextSize(100);
+            txtDescriptionBarcode.setTextSize(18);
+            tvBarcode.setTextSize(120);
             barcodeScale = false;
-
-            RelativeLayout.LayoutParams layoutParamsBarcode = new RelativeLayout.LayoutParams(500, 350);
-            layoutParamsBarcode.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-            layoutParamsBarcode.addRule(RelativeLayout.CENTER_HORIZONTAL);
-            tvBarcode.setLayoutParams(layoutParamsBarcode);
-            tvBarcode.setPadding(0, 25, 0, 0);
+            tvBarcode.setPadding(0, 20, 20, 20);
         } else {
-            tvBarcode.setTextSize(TypedValue.COMPLEX_UNIT_MM, 40);
-            RelativeLayout.LayoutParams layoutParamsBarcode = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 725);
-            layoutParamsBarcode.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-            layoutParamsBarcode.addRule(RelativeLayout.CENTER_IN_PARENT);
-            tvBarcode.setLayoutParams(layoutParamsBarcode);
-            tvBarcode.setPadding(0, 50, 0, 0);
+            txtDescriptionBarcode.setTextSize(0);
+            tvBarcode.setTextSize(200);
+            tvBarcode.setPadding(0, 20, 20, 20);
             barcodeScale = true;
         }
-
-    }
-
-    @SuppressLint("InflateParams")
-    public void zoomMap(View v) {
-        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
-        View mView = getLayoutInflater().inflate(R.layout.dialog_custom_layout, null);
-        PhotoView photoView = mView.findViewById(R.id.imageView);
-        int id = v.getId();
-        int res = R.drawable.firstf;
-        if (id == R.id.map2ImageView) {
-            res = R.drawable.secondf;
-        } else if (id == R.id.map3ImageView) {
-            res = R.drawable.thirdf;
-        }
-        photoView.setImageResource(res);
-        mBuilder.setView(mView);
-        AlertDialog mDialog = mBuilder.create();
-        mDialog.show();
     }
 
     long timeUp;
-    int cnt = 1;
+
     String contentText;
+    boolean dependence = false;
 
     private void planningPush() {
         Timer timer = new Timer();
@@ -152,59 +119,76 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot i : dataSnapshot.getChildren()) {
-
-                    // забираю все данные из event из БД
-
-                    String name = (String) i.child("name").getValue();
-                    boolean is_optional = (boolean) i.child("is_optional").getValue();
-                    String place = (String) i.child("place").getValue();
-                    String startDate = (String) i.child("dateStart").getValue();
-
-                    // высчитываю время
-                    try {
-                        timeUp = Objects.requireNonNull(format.parse(Objects.requireNonNull(startDate))).getTime();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    // нахожу разницу
-                    long diff = timeUp - System.currentTimeMillis();
-
-                    try {
-                        if (is_optional) {
-                            contentText = "Просим по желанию пройти в " + place + " через 10 минут там начнется мероприятие " + name;
-                        } else {
-                            contentText = "Просим пройти в " + place + " через 10 минут там начнется обязательное мероприятие " + name;
-
-                        }
-                        // создаю пуш
-                        timer.schedule(new TimerTask() {
-                            @Override
-                            public void run() {
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        createPush("Опопвещение о мероприятии");
-
-                                        cnt += 1;
-                                        Log.d("WTF", "I AM TIRED");
-                                    }
-                                });
+                    for (DataSnapshot j : i.child("invitedgroups").getChildren()) {
+                        String invitedgroup = (String) j.getValue();
+                        for (String s : groups) {
+                            if (s.equals(invitedgroup)) {
+                                dependence = true;
+                                break;
                             }
-                        }, diff - 600000);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        }
                     }
+                    if (dependence) {
 
+                        // забираю все данные из event из БД
+
+                        id = (int) i.child("id").getValue();
+                        String name = (String) i.child("name").getValue();
+                        boolean is_optional = (boolean) i.child("is_optional").getValue();
+                        String place = (String) i.child("place").getValue();
+                        String startDate = (String) i.child("dateStart").getValue();
+
+                        // высчитываю время
+                        try {
+                            timeUp = Objects.requireNonNull(format.parse(Objects.requireNonNull(startDate))).getTime();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        // нахожу разницу
+                        long diff = timeUp - System.currentTimeMillis();
+
+                        try {
+                            if (is_optional) {
+                                contentText = "Просим по желанию пройти в " + place + " через 10 минут там начнется мероприятие " + name;
+                            } else {
+                                contentText = "Просим пройти в " + place + " через 10 минут там начнется обязательное мероприятие " + name;
+
+                            }
+                            // создаю пуш
+                            timer.schedule(new TimerTask() {
+                                @Override
+                                public void run() {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            createPush("Опопвещение о мероприятии");
+
+
+                                            Log.d("WTF", "I AM TIRED");
+                                        }
+                                    });
+                                }
+                            }, diff - 600000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
                 }
             }
+
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         };
+
         userRef.addListenerForSingleValueEvent(valueEventListener);
+
     }
+
+
 
     private void createPush(String title) {
         NotificationCompat.Builder mBuilder =
@@ -236,8 +220,6 @@ public class MainActivity extends AppCompatActivity {
             mBuilder.setChannelId(channelId);
         }
 
-        mNotificationManager.notify(cnt, mBuilder.build());
+        mNotificationManager.notify(id, mBuilder.build());
     }
 }
-
-
